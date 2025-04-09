19b1ad878f470b3d366dab716119a6a18a9bb1f6
Jackson Charnock (RIT Student) <eec9960@rit.edu>
2025-04-09 17:41:33 -0400

    updating with completed work

---
+++ l5_pt2/Core/Src/sharedtasknames.h
@@ -0,0 +1,17 @@
+/*
+ * sharedtasknames.h
+ *
+ *  Created on: Mar 26, 2025
+ *      Author: eec9960
+ */
+
+#ifndef SRC_SHAREDTASKNAMES_H
+#define SRC_SHAREDTASKNAMES_H
+
+#include "cmsis_os.h"
+#include <stdbool.h>
+
+	extern osSemaphoreId myBinarySem01Handle;
+	extern osSemaphoreId myBinarySem02Handle;
+
+#endif /* SRC SHAREDTASKNAMES H */
